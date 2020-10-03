2c57e78f9e07ae91690b3785cc15ef800b80e485
Sonali Sharma <sssona09@gmail.com>
2020-10-03 14:43:51 +1000

    calculate car emissions using csv

--- emissions.py
+++ emissions.py
@@ -1,20 +1,26 @@
 import csv
 '''
-#def emissions(distance_travelled, make, vehicle_class)
 car make choices: Acura, Alfa Romeo, Aston Martin, Audi, Bentley, BMW, Bugatti, Buick, Cadillac,
 Chevrolet, Chrysler, Dodge, Ford, Genesis, GMC, Honda, Hyundai, Infiniti, Jaguar, Jeep,
 Kia, Lamborghini, Land Rover, Lexus, Lincoln, Maserati, Mazda, Mercedes-Benz, MINI, Mitsubishi,
 Nissan, Porsche, Ram, Rolls-Royce, Subaru, Toyota, Volkswagen, Volvo
 
-vehicle class options: SUV: Standard, SUV: Small, Mid-size, Full-size, Compact, Subcompact, Station wagon: Small
+vehicle class options: SUV: Standard, SUV: Small, Mid-size, Full-size, Compact, Subcompact, Station wagon: Small, Minivan,
+Two-seater, Minicompact, Pickup truck: Small, Pickup truck: Mid-size
 '''
-with open('fuel_consumption.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
+def emissions(distance_travelled, make, vehicle_class):
+    with open('fuel_consumption.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
 
-    make = "Acura"
-    vehicle_class = ""
+        for line in csv_reader:
+            #get fuel consumption combined (L/100 Km)
+            if (line[1] == make and line[3] == vehicle_class):
+                fuel_economy = line[10]
+        
+        try: 
+            fuel_emissions = (float(fuel_economy) * 2.33) * 10 * distance_travelled
+            return fuel_emissions
+        except:
+            return {}
 
-    for line in csv_reader:
-        #get fuel consumption combined (L/100 Km)
-        if (line[1] == make and line[3] == vehicle_class):
-            print(line[10])
+#print(emissions(36, "Audi", "Subcompact"))
